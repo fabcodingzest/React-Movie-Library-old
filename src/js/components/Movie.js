a3c2d3e339a577cb00bf769d5ae0e508055e2e55
fabcodingzest <fabrizvi786@gmail.com>
2020-01-28 18:28:55 +0530

    Improved UI and added react-rating library(buggy)

--- src/js/components/Movie.js
+++ src/js/components/Movie.js
@@ -1,17 +1,42 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import styled from "styled-components";
+import Rating from "react-rating";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Card = styled.div`
+  width: 200px;
+  margin-bottom: 6rem;
+  margin: 0.5rem;
+  padding: 0.5rem;
+  color: black;
+  background-color: none;
+  color: #fff;
+  font-family: Roboto, sans-serif;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    background-color: #eee;
+    color: black;
+    box-shadow: 1px 2px 15px #fff;
+    transform: scale(1.1);
+  }
+`;
 
 const Movie = props => {
   const { movie } = props;
   return (
     <Card>
-      <Card.Img
-        variant="top"
+      <img
         src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
+        alt={movie.original_title}
       />
-      <Card.Body>
-        <Card.Title>{movie.original_title}</Card.Title>
-      </Card.Body>
+      <h4>{movie.original_title}</h4>
+
+      <Rating
+        placeholderRating={movie.vote_average / 2}
+        emptySymbol="fa fa-star-o fa-2x"
+        fullSymbol="fa fa-star fa-2x"
+        readonly
+      />
     </Card>
   );
 };
--- src/js/components/MovieList.js
+++ src/js/components/MovieList.js
@@ -2,6 +2,17 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { requestMovies } from "../actions/index";
 import Movie from "./Movie";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: space-around;
+  width: 100vw;
+  text-align: center;
+  padding: 2rem;
+`;
 
 const mapStateToProps = state => {
   return { movies: state.movies };
@@ -14,9 +25,11 @@ const MovieList = props => {
 
   return (
     <div>
-      {props.movies.map(movie => (
-        <Movie key={movie.original_title} movie={movie} />
-      ))}
+      <Wrapper>
+        {props.movies.map(movie => (
+          <Movie key={movie.original_title} movie={movie} />
+        ))}
+      </Wrapper>
     </div>
   );
 };
