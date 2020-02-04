f974d10536146c63ef7e55ebb4e987b8bfeff982
fabcodingzest <fabrizvi786@gmail.com>
2020-02-04 20:57:13 +0530

    No work Done today as well, FML

--- src/js/containers/Search.js
+++ src/js/containers/Search.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { searchRequest } from "../actions/index";
+
+const mapStateToProps = state => {
+  return {
+    movies: state.movies
+  };
+};
 
 const Search = () => {
+  const { searchRequest, movies } = props;
+  useEffect(() => {
+    searchRequest();
+  }, [requestMovies]);
   return <div></div>;
 };
-export default Search;
+export default connect(mapStateToProps, { requestMovies })(Search);
